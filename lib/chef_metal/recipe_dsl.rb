ae0fb372bd4bf497785b25f9291f781869a9fc2a
Curtis Stewart <cstewart@momentumsi.com>
2014-03-19 19:44:46 -0500

    Updates to match current master

--- lib/chef_metal/recipe_dsl.rb
+++ lib/chef_metal/recipe_dsl.rb
@@ -9,25 +9,5 @@ class Chef
     def with_provisioner_options(provisioner_options, &block)
       ChefMetal.with_provisioner_options(provisioner_options, &block)
     end
-
-    def with_vagrant_cluster(cluster_path, &block)
-      ChefMetal.with_vagrant_cluster(cluster_path, &block)
-    end
-
-    def with_vagrant_box(box_name, vagrant_options = {}, &block)
-      ChefMetal.with_vagrant_box(box_name, vagrant_options, &block)
-    end
-
-    def with_fog_provisioner(options = {}, &block)
-      ChefMetal.with_provisioner(ChefMetal::Provisioner::FogProvisioner.new(options, &block))
-    end
-
-    def with_fog_ec2_provisioner(options = {}, &block)
-      with_fog_provisioner({ :provider => 'AWS' }.merge(options), &block)
-    end
-
-    def with_fog_openstack_provisioner(options = {}, &block)
-      with_fog_provisioner({ :provider => 'OpenStack' }.merge(options), &block)
-    end
   end
 end
