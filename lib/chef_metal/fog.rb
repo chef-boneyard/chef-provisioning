531a94129dbb6714854cdb8dc60f9d030a60ee62
John Keiser <john@johnkeiser.com>
2014-03-25 14:53:22 -0700

    Make with_fog_provisioner <block> work

--- lib/chef_metal/fog.rb
+++ lib/chef_metal/fog.rb
@@ -6,7 +6,7 @@ require 'chef_metal/provisioner/fog_provisioner'
 class Chef
   class Recipe
     def with_fog_provisioner(options = {}, &block)
-      ChefMetal.with_provisioner(ChefMetal::Provisioner::FogProvisioner.new(options, &block))
+      ChefMetal.with_provisioner(ChefMetal::Provisioner::FogProvisioner.new(options), &block)
     end
 
     def with_fog_ec2_provisioner(options = {}, &block)
