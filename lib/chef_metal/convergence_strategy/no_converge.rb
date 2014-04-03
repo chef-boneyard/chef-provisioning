ff1054b8862ea197c62c1d9da3824283dfbbd3e7
John Keiser <jkeiser@opscode.com>
2014-04-03 13:05:11 -0700

    Add NoConverge convergence strategy that just makes a node

---
+++ lib/chef_metal/convergence_strategy/no_converge.rb
@@ -0,0 +1,31 @@
+require 'chef_metal/convergence_strategy'
+require 'pathname'
+require 'cheffish'
+
+module ChefMetal
+  class ConvergenceStrategy
+    class NoConverge < ConvergenceStrategy
+      attr_reader :client_rb_path
+      attr_reader :client_pem_path
+
+      def setup_convergence(action_handler, machine, machine_resource)
+        # Save the node
+        ChefMetal.inline_resource(action_handler) do
+          # TODO strip automatic attributes first so we don't race with "current state"
+          chef_node machine.node['name'] do
+            chef_server machine_resource.chef_server
+            raw_json machine.node
+          end
+        end
+      end
+
+      def delete_chef_objects(action_handler, node)
+        ChefMetal.inline_resource(action_handler) do
+          chef_node node['name'] do
+            action :delete
+          end
+        end
+      end
+    end
+  end
+end
