f5d73a6114145e1f00ff12d1b3c610968007a6c9
John Keiser <jkeiser@opscode.com>
2014-04-04 13:03:29 -0700

    Move Fog provisioner to chef-metal-fog

--- cookbooks/myapp/recipes/ec2.rb
+++ cookbooks/myapp/recipes/ec2.rb
@@ -1,4 +1,4 @@
-require 'chef_metal/fog'
+require 'chef_metal_fog'
 
 ec2testdir = File.expand_path('~/ec2test')
 
--- lib/chef_metal/provisioner_init/fog_init.rb
+++
@@ -1,4 +0,0 @@
-require 'chef_metal/provisioner/fog_provisioner'
-
-ChefMetal.add_registered_provisioner_class("fog",
-  ChefMetal::Provisioner::FogProvisioner)
