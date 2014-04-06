e589d97a39d3ef81fe0de4b57af685493ed240ee
Irving Popovetsky <irving@getchef.com>
2014-04-06 11:48:59 -0700

    Add a SUPER SIMPLE machine_execute resource

---
+++ lib/chef/resource/machine_execute.rb
@@ -0,0 +1,23 @@
+require 'chef/resource/lwrp_base'
+require 'chef_metal'
+require 'chef_metal/machine'
+require 'chef_metal/provisioner'
+
+class Chef::Resource::MachineExecute < Chef::Resource::LWRPBase
+  self.resource_name = 'machine_execute'
+
+  def initialize(*args)
+    super
+    @chef_server = Cheffish.enclosing_chef_server
+    @provisioner = ChefMetal.enclosing_provisioner
+  end
+
+  actions :run
+  default_action :run
+
+  attribute :command, :kind_of => String, :name_attribute => true
+  attribute :machine, :kind_of => [String, ChefMetal::Machine]
+
+  attribute :chef_server, :kind_of => Hash
+  attribute :provisioner, :kind_of => ChefMetal::Provisioner
+end
