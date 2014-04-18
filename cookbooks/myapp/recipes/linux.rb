60f8e64a36fe3ca8663b9c47ce6732fa413450ef
John Keiser <john@johnkeiser.com>
2014-04-17 23:41:26 -0700

    Remove non-universal provisioner options

--- cookbooks/myapp/recipes/linux.rb
+++ cookbooks/myapp/recipes/linux.rb
@@ -1,7 +1,7 @@
 # Use a Linux image
 vagrant_box 'precise64' do
   url 'http://files.vagrantup.com/precise64.box'
-  provisioner_options 'vagrant_config' => <<EOM
-    config.vm.synced_folder (ENV['SRC_DIR'] || "#{ENV['HOME']}/oc/code/opscode"), "/mnt/host_src"
-EOM
+#  provisioner_options 'vagrant_config' => <<EOM
+#    config.vm.synced_folder (ENV['SRC_DIR'] || "#{ENV['HOME']}/oc/code/opscode"), "/mnt/host_src"
+#EOM
 end
