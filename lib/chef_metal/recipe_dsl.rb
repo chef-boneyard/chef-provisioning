e2c8d762cd1669ae8ed7b058da5ad4cc1d09a268
John Keiser <jkeiser@opscode.com>
2014-04-29 13:19:19 -0700

    Create a machine_batch :converge by default

--- lib/chef_metal/recipe_dsl.rb
+++ lib/chef_metal/recipe_dsl.rb
@@ -1,4 +1,6 @@
 require 'chef_metal/chef_run_data'
+require 'chef_metal/chef_run_listener'
+require 'chef/resource_collection'
 
 class Chef
   class Recipe
@@ -35,7 +37,35 @@ class Chef
 
   class RunContext
     def chef_metal
-      @chef_metal ||= ChefMetal::ChefRunData.new
+      @chef_metal ||= begin
+        run_data = ChefMetal::ChefRunData.new
+        events.register(ChefMetal::ChefRunListener.new(self))
+        run_data
+      end
+    end
+  end
+
+  Chef::Client.when_run_starts do |run_status|
+    # Pulling on cheffish_run_data makes it initialize right now
+    run_status.run_context.chef_metal
+  end
+
+  # Make sure ResourceCollection has insert_at
+  class ResourceCollection
+    if !method_defined?(:insert_at)
+      def insert_at(index, *resources)
+        resources.each do |resource|
+          is_chef_resource(resource)
+        end
+        @resources.insert(index, *resources)
+        # update name -> location mappings and register new resource
+        @resources_by_name.each_key do |key|
+          @resources_by_name[key] += resources.size if @resources_by_name[key] >= index
+        end
+        resources.each_with_index do |resource, i|
+          @resources_by_name[resource.to_s] = index + i
+        end
+      end
     end
   end
 end
