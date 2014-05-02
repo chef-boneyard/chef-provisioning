9bd5dc42d1b373b57a5972dfd911d8d646592d48
John Keiser <jkeiser@opscode.com>
2014-05-02 11:02:01 -0700

    Move Recipe DSL to Chef::DSL::Recipe

--- lib/chef_metal_fog.rb
+++ lib/chef_metal_fog.rb
@@ -4,17 +4,19 @@ require 'chef/provider/fog_key_pair'
 require 'chef_metal_fog/fog_provisioner'
 
 class Chef
-  class Recipe
-    def with_fog_provisioner(options = {}, &block)
-      run_context.chef_metal.with_provisioner(ChefMetalFog::FogProvisioner.new(options), &block)
-    end
+  module DSL
+    module Recipe
+      def with_fog_provisioner(options = {}, &block)
+        run_context.chef_metal.with_provisioner(ChefMetalFog::FogProvisioner.new(options), &block)
+      end
 
-    def with_fog_ec2_provisioner(options = {}, &block)
-      with_fog_provisioner({ :provider => 'AWS' }.merge(options), &block)
-    end
+      def with_fog_ec2_provisioner(options = {}, &block)
+        with_fog_provisioner({ :provider => 'AWS' }.merge(options), &block)
+      end
 
-    def with_fog_openstack_provisioner(options = {}, &block)
-      with_fog_provisioner({ :provider => 'OpenStack' }.merge(options), &block)
+      def with_fog_openstack_provisioner(options = {}, &block)
+        with_fog_provisioner({ :provider => 'OpenStack' }.merge(options), &block)
+      end
     end
   end
 end
