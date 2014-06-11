95804d1ffd6e92ae35a6a188cbd99445d6fbcb41
Thom May <thom_may@external.mckinsey.com>
2014-06-11 16:58:08 +0100

    add openstack driver

--- lib/chef_metal_fog.rb
+++ lib/chef_metal_fog.rb
@@ -1,3 +1,4 @@
 require 'chef_metal'
 require 'chef_metal_fog/drivers/rackspace'
+require 'chef_metal_fog/drivers/openstack'
 require 'chef_metal_fog/recipe_dsl'
---
+++ lib/chef_metal_fog/drivers/openstack.rb
@@ -0,0 +1,35 @@
+require 'chef_metal_fog/fog_driver'
+module ChefMetalFog
+  module Drivers
+    class OpenStack < ChefMetalFog::FogDriver
+
+      def creator
+        compute_options[:openstack_username]
+      end
+
+      def self.compute_options_for(provider, id, config)
+        new_compute_options = {}
+        new_compute_options[:provider] = provider
+        new_config = { :driver_options => { :compute_options => new_compute_options }}
+        new_defaults = {
+          :driver_options => { :compute_options => {} },
+          :machine_options => { :bootstrap_options => {} }
+        }
+        result = Cheffish::MergedConfig.new(new_config, config, new_defaults)
+
+        new_compute_options[:openstack_auth_url] = id if (id && id != '')
+        credential = Fog.credentials
+
+        new_compute_options[:openstack_username] ||= credential[:openstack_username]
+        new_compute_options[:openstack_api_key] ||= credential[:openstack_api_key]
+        new_compute_options[:openstack_auth_url] ||= credential[:openstack_auth_url]
+        new_compute_options[:openstack_tenant] ||= credential[:openstack_tenant]
+
+        id = result[:driver_options][:compute_options][:openstack_auth_url]
+
+        [result, id]
+      end
+
+    end
+  end
+end
