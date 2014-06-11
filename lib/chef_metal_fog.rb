26206117055b71320b2024f6a623aeb7e548ade3
Thom May <thom_may@external.mckinsey.com>
2014-06-11 16:59:45 +0100

    add DigitalOcean driver

--- lib/chef_metal_fog.rb
+++ lib/chef_metal_fog.rb
@@ -1,4 +1,5 @@
 require 'chef_metal'
 require 'chef_metal_fog/drivers/rackspace'
 require 'chef_metal_fog/drivers/openstack'
+require 'chef_metal_fog/drivers/digital_ocean'
 require 'chef_metal_fog/recipe_dsl'
