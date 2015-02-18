915eac36d868ce81abc889abfaa2f25882d218c3
John Keiser <john@johnkeiser.com>
2015-02-18 13:19:45 -0800

    Add chef_version, prerelease and install_sh_arguments to InstallSh

--- lib/chef/provisioning/convergence_strategy/install_sh.rb
+++ lib/chef/provisioning/convergence_strategy/install_sh.rb
@@ -15,13 +15,26 @@ module Provisioning
         super(convergence_options, config)
         @install_sh_url = convergence_options[:install_sh_url] || 'http://www.chef.io/chef/install.sh'
         @install_sh_path = convergence_options[:install_sh_path] || '/tmp/chef-install.sh'
+        @chef_version = convergence_options[:chef_version]
+        @prerelease = convergence_options[:prerelease]
+        @install_sh_arguments = convergence_options[:install_sh_arguments]
         @bootstrap_env = convergence_options[:bootstrap_proxy] ? "http_proxy=#{convergence_options[:bootstrap_proxy]}" : ""
         @chef_client_timeout = convergence_options.has_key?(:chef_client_timeout) ? convergence_options[:chef_client_timeout] : 120*60 # Default: 2 hours
       end
 
+      attr_reader :chef_version
+      attr_reader :prerelease
       attr_reader :install_sh_url
       attr_reader :install_sh_path
+      attr_reader :install_sh_arguments
       attr_reader :bootstrap_env
+
+      def install_sh_command_line
+        arguments = install_sh_arguments ? " #{install_sh_arguments}" : ""
+        arguments << " -v #{chef_version}" if chef_version
+        arguments << " -p" if prerelease
+        "bash -c '#{bootstrap_env} bash #{install_sh_path}#{arguments}'"
+      end
 
       def setup_convergence(action_handler, machine)
         super
@@ -31,7 +44,7 @@ module Provisioning
           # TODO ssh verification of install.sh before running arbtrary code would be nice?
           @@install_sh_cache[install_sh_url] ||= Net::HTTP.get(URI(install_sh_url))
           machine.write_file(action_handler, install_sh_path, @@install_sh_cache[install_sh_url], :ensure_dir => true)
-          machine.execute(action_handler, "bash -c '#{bootstrap_env} bash #{install_sh_path}'")
+          machine.execute(action_handler, install_sh_command_line)
         end
       end
 
