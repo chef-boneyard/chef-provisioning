fa5d352247028dea04b8ecd1aeffa0591ddd1503
John Keiser <john@johnkeiser.com>
2015-03-29 14:31:29 -0700

    Add the ability bind to a specific port; create a background thread to kick SSH forward so it processes forwarded port packets.

--- lib/chef/provisioning/transport/ssh.rb
+++ lib/chef/provisioning/transport/ssh.rb
@@ -39,6 +39,8 @@ module Provisioning
         @ssh_options = ssh_options
         @options = options
         @config = global_config
+        @using_session = 0
+        @session_thread_errors = []
       end
 
       attr_reader :host
@@ -52,42 +54,44 @@ module Provisioning
         stdout = ''
         stderr = ''
         exitstatus = nil
-        session # grab session outside timeout, it has its own timeout
-        with_execute_timeout(execute_options) do
-          channel = session.open_channel do |channel|
-            # Enable PTY unless otherwise specified, some instances require this
-            unless options[:ssh_pty_enable] == false
-              channel.request_pty do |chan, success|
-                 raise "could not get pty" if !success && options[:ssh_pty_enable]
+        # grab session outside timeout, it has its own timeout
+        with_session do |session|
+          with_execute_timeout(execute_options) do
+            channel = session.open_channel do |channel|
+              # Enable PTY unless otherwise specified, some instances require this
+              unless options[:ssh_pty_enable] == false
+                channel.request_pty do |chan, success|
+                   raise "could not get pty" if !success && options[:ssh_pty_enable]
+                end
+              end
+
+              channel.exec("#{options[:prefix]}#{command}") do |ch, success|
+                raise "could not execute command: #{command.inspect}" unless success
+
+                channel.on_data do |ch2, data|
+                  stdout << data
+                  stream_chunk(execute_options, data, nil)
+                end
+
+                channel.on_extended_data do |ch2, type, data|
+                  stderr << data
+                  stream_chunk(execute_options, nil, data)
+                end
+
+                channel.on_request "exit-status" do |ch, data|
+                  exitstatus = data.read_long
+                end
               end
             end
 
-            channel.exec("#{options[:prefix]}#{command}") do |ch, success|
-              raise "could not execute command: #{command.inspect}" unless success
-
-              channel.on_data do |ch2, data|
-                stdout << data
-                stream_chunk(execute_options, data, nil)
-              end
-
-              channel.on_extended_data do |ch2, type, data|
-                stderr << data
-                stream_chunk(execute_options, nil, data)
-              end
-
-              channel.on_request "exit-status" do |ch, data|
-                exitstatus = data.read_long
-              end
-            end
+            channel.wait
           end
 
-          channel.wait
+          Chef::Log.info("Completed #{command} on #{username}@#{host}: exit status #{exitstatus}")
+          Chef::Log.debug("Stdout was:\n#{stdout}") if stdout != '' && !options[:stream] && !options[:stream_stdout] && config[:log_level] != :debug
+          Chef::Log.info("Stderr was:\n#{stderr}") if stderr != '' && !options[:stream] && !options[:stream_stderr] && config[:log_level] != :debug
+          SSHResult.new(command, execute_options, stdout, stderr, exitstatus)
         end
-
-        Chef::Log.info("Completed #{command} on #{username}@#{host}: exit status #{exitstatus}")
-        Chef::Log.debug("Stdout was:\n#{stdout}") if stdout != '' && !options[:stream] && !options[:stream_stdout] && config[:log_level] != :debug
-        Chef::Log.info("Stderr was:\n#{stderr}") if stderr != '' && !options[:stream] && !options[:stream_stderr] && config[:log_level] != :debug
-        SSHResult.new(command, execute_options, stdout, stderr, exitstatus)
       end
 
       def read_file(path)
@@ -117,21 +121,23 @@ module Provisioning
       end
 
       def upload_file(local_path, path)
-        execute("mkdir -p #{File.dirname(path)}").error!
-        if options[:prefix]
-          # Make a tempfile on the other side, upload to that, and sudo mv / chown / etc.
-          remote_tempfile = "/tmp/#{File.basename(path)}.#{Random.rand(2**32)}"
-          Chef::Log.debug("Uploading #{local_path} to #{remote_tempfile} on #{username}@#{host}")
-          Net::SCP.new(session).upload!(local_path, remote_tempfile)
-          begin
-            execute("mv #{remote_tempfile} #{path}").error!
-          rescue
-            # Clean up if we were unable to move
-            execute("rm #{remote_tempfile}").error!
+        with_session do |session|
+          execute("mkdir -p #{File.dirname(path)}").error!
+          if options[:prefix]
+            # Make a tempfile on the other side, upload to that, and sudo mv / chown / etc.
+            remote_tempfile = "/tmp/#{File.basename(path)}.#{Random.rand(2**32)}"
+            Chef::Log.debug("Uploading #{local_path} to #{remote_tempfile} on #{username}@#{host}")
+            Net::SCP.new(session).upload!(local_path, remote_tempfile)
+            begin
+              execute("mv #{remote_tempfile} #{path}").error!
+            rescue
+              # Clean up if we were unable to move
+              execute("rm #{remote_tempfile}").error!
+            end
+          else
+            Chef::Log.debug("Uploading #{local_path} to #{path} on #{username}@#{host}")
+            Net::SCP.new(session).upload!(local_path, path)
           end
-        else
-          Chef::Log.debug("Uploading #{local_path} to #{path} on #{username}@#{host}")
-          Net::SCP.new(session).upload!(local_path, path)
         end
       end
 
@@ -177,6 +183,10 @@ module Provisioning
 
 
       def disconnect
+        if @session_thread
+          @session_thread.kill
+          @session_thread = nil
+        end
         if @session
           begin
             Chef::Log.debug("Closing SSH session on #{username}@#{host}")
@@ -206,18 +216,90 @@ module Provisioning
 
       def session
         @session ||= begin
-          ssh_start_opts = { timeout:10 }.merge(ssh_options)
+          ssh_start_opts = { timeout:10, logger: Chef::Log.logger, verbose: log_level }.merge(ssh_options)
           Chef::Log.debug("Opening SSH connection to #{username}@#{host} with options #{ssh_start_opts.dup.tap {
                               |ssh| ssh.delete(:key_data) }.inspect}")
           # Small initial connection timeout (10s) to help us fail faster when server is just dead
+          session = nil
           begin
-            if gateway? then gateway.ssh(host, username, ssh_start_opts)
-            else Net::SSH.start(host, username, ssh_start_opts)
+            if gateway?
+              session = gateway.ssh(host, username, ssh_start_opts)
+            else
+              session = Net::SSH.start(host, username, ssh_start_opts)
             end
           rescue Timeout::Error
             Chef::Log.debug("Timed out connecting to SSH: #{$!}")
             raise InitialConnectTimeout.new($!)
           end
+          @session_thread ||= Thread.new do
+            while @session
+              # If others are doing SSH things, we let them handle processing; this
+              # increases the chances they will receive appropriate exceptions.
+              if @using_session == 0
+                begin
+                  # If there was something to process, keep processing
+                  next if @session.process?(0)
+                rescue
+                  # If there is an error, we save it off so that a real thread can capture it.
+                  @session_thread_errors << $!
+                  next
+                end
+                sleep(0.1)
+              end
+            end
+          end
+          session
+        end
+      end
+
+      #
+      # Tell the transport that we are going to do SSH things; this pauses the
+      # main loop thread that is pushing SSH forward in the background, so that
+      # any SSH errors will be raised by the current thread.  It ain't perfect,
+      # but as long as only one SSH thing is actually happening, it will do the
+      # errors right.
+      #
+      def with_session(&block)
+        raise_unhandled_errors
+        @using_session += 1
+        begin
+          block.call(session)
+        ensure
+          @using_session -= 1
+        end
+      end
+
+      class UnhandledSSHError < StandardError
+        def initialize(original_error)
+          @original_error = original_error
+        end
+
+        attr_reader :original_error
+
+        def message
+          original_error.message
+        end
+      end
+
+      def raise_unhandled_errors
+        if !@session_thread_errors.empty?
+          error = @session_thread_errors.shift
+          Chef::Log.debug("SSH error caught: #{error.backtrace}")
+          raise UnhandledSSHError.new(error)
+        end
+      end
+
+      # :debug is REALLY terribly verbose and almost never of interest.  If we
+      # want verbose, we need to give Chef a new log level like :firehose
+      # TODO log_level :firehose
+      def log_level
+        case Chef::Log.level
+        when :info
+          :warn
+        when :debug
+          :info
+        else
+          Chef::Log.level
         end
       end
 
@@ -247,21 +329,23 @@ module Provisioning
       end
 
       def do_download(path, local_path)
-        channel = Net::SCP.new(session).download(path, local_path)
-        begin
-          channel.wait
-          Chef::Log.debug "SCP completed for: #{path} to #{local_path}"
-        rescue Net::SCP::Error => e
-          Chef::Log.error "Error with SCP: #{e}"
-          # TODO we need a way to distinguish between "directory or file does not exist" and "SCP did not finish successfully"
-          nil
-        ensure
-          # ensure the channel is closed
-          channel.close
-          channel.wait
-        end
+        with_session do |session|
+          channel = Net::SCP.new(session).download(path, local_path)
+          begin
+            channel.wait
+            Chef::Log.debug "SCP completed for: #{path} to #{local_path}"
+          rescue Net::SCP::Error => e
+            Chef::Log.error "Error with SCP: #{e}"
+            # TODO we need a way to distinguish between "directory or file does not exist" and "SCP did not finish successfully"
+            nil
+          ensure
+            # ensure the channel is closed
+            channel.close
+            channel.wait
+          end
 
-        nil
+          nil
+        end
       end
 
       class SSHResult
@@ -345,81 +429,101 @@ module Provisioning
 
       # Forwards packets from remote clients to a local server.
       def forward_remote_port(local_port, local_host, remote_port, remote_host)
-        # This bit is from the documentation.
-        if session.forward.respond_to?(:active_remote_destinations)
-          # active_remote_destinations tells us exactly what remotes the current
-          # ssh session is *actually* tracking.  If multiple people share this
-          # session and set up their own remotes, this will prevent us from
-          # overwriting them.
-          actual_remote_port, actual_remote_host = session.forward.active_remote_destinations[[local_port, local_host]]
+        with_session do |session|
+          # This bit is from the documentation.
+          if session.forward.respond_to?(:active_remote_destinations)
+            # active_remote_destinations tells us exactly what remotes the current
+            # ssh session is *actually* tracking.  If multiple people share this
+            # session and set up their own remotes, this will prevent us from
+            # overwriting them.
+            actual_remote_port, actual_remote_host = session.forward.active_remote_destinations[[local_port, local_host]]
 
-          if !actual_remote_port
-            Chef::Log.debug("Forwarding local server #{local_host}:#{local_port} to #{remote_host}:#{remote_port} on #{username}@#{self.host}")
+            if !actual_remote_port
+              Chef::Log.debug("Forwarding #{remote_host}:#{remote_port} on #{username}@#{self.host} to local server #{local_host}:#{local_port}")
 
-            session.forward.remote(local_port, local_host, remote_port, remote_host) do |new_remote_port, new_remote_host|
-  						actual_remote_host = new_remote_host
-              actual_remote_port = new_remote_port || :error
-              :no_exception # I'll take care of it myself, thanks
+              session.forward.remote(local_port, local_host, remote_port, remote_host) do |new_remote_port, new_remote_host|
+    						actual_remote_host = new_remote_host
+                actual_remote_port = new_remote_port || :error
+                :no_exception # I'll take care of it myself, thanks
+              end
+              # Kick SSH until we get a response
+              session.loop(0.1) { !actual_remote_port }
+              if actual_remote_port == :error
+                return nil
+              end
             end
-            # Kick SSH until we get a response
-            session.loop { !actual_remote_port }
-            if actual_remote_port == :error
-              return nil
+            [ actual_remote_port, actual_remote_host ]
+
+          else
+            # If active_remote_destinations isn't on net-ssh, we stash our own list
+            # of ports *we* have forwarded on the connection, and hope that we are
+            # right.
+            # TODO let's remove this when net-ssh 2.9.2 is old enough, and
+            # bump the required net-ssh version.
+
+            @forwarded_remote_ports ||= {}
+            actual_remote_port, actual_remote_host = @forwarded_remote_ports[[local_port, local_host]]
+            if !actual_remote_port
+              Chef::Log.debug("Forwarding #{remote_host}:#{remote_port} on #{username}@#{self.host} to local server #{local_host}:#{local_port}")
+              old_active_remotes = session.forward.active_remotes
+              session.forward.remote(local_port, local_host, remote_port, remote_host)
+              session.loop { !(session.forward.active_remotes.length > old_active_remotes.length) }
+              actual_remote_port, actual_remote_host = (session.forward.active_remotes - old_active_remotes).first
+              @forwarded_remote_ports[[local_port, local_host]] = [ actual_remote_port, actual_remote_host ]
             end
+            [ actual_remote_port, actual_remote_host ]
           end
-          [ actual_remote_port, actual_remote_host ]
-
-        else
-          # If active_remote_destinations isn't on net-ssh, we stash our own list
-          # of ports *we* have forwarded on the connection, and hope that we are
-          # right.
-          # TODO let's remove this when net-ssh 2.9.2 is old enough, and
-          # bump the required net-ssh version.
-
-          @forwarded_remote_ports ||= {}
-          actual_remote_port, actual_remote_host = @forwarded_remote_ports[[local_port, local_host]]
-          if !actual_remote_port
-            Chef::Log.debug("Forwarding local server #{local_host}:#{local_port} to #{remote_host}:#{remote_port} on #{username}@#{self.host}")
-            old_active_remotes = session.forward.active_remotes
-            session.forward.remote(local_port, local_host, remote_port, remote_host)
-            session.loop { !(session.forward.active_remotes.length > old_active_remotes.length) }
-            actual_remote_port, actual_remote_host = (session.forward.active_remotes - old_active_remotes).first
-            @forwarded_remote_ports[[local_port, local_host]] = [ actual_remote_port, actual_remote_host ]
-          end
-          [ actual_remote_port, actual_remote_host ]
         end
       end
 
       # Forwards packets from local clients to a remote server.
       def forward_local_port(local_port, local_host, remote_port, remote_host)
-        # This bit is from the documentation.
-        if session.forward.respond_to?(:active_local_destinations)
-          # active_local_destinations tells us exactly what locals the current
-          # ssh session is *actually* tracking.  If multiple people share this
-          # session and set up their own locals, this will prevent us from
-          # overwriting them.
+        with_session do |session|
+          # This bit is from the documentation.
+          if session.forward.respond_to?(:active_local_destinations)
+            # active_local_destinations tells us exactly what locals the current
+            # ssh session is *actually* tracking.  If multiple people share this
+            # session and set up their own locals, this will prevent us from
+            # overwriting them.
 
-          actual_local_port, actual_local_host = session.forward.active_local_destinations[[local_port, local_host]]
-        else
-          # If active_local_destinations isn't on net-ssh, we stash our own list
-          # of ports *we* have forwarded on the connection, and hope that we are
-          # right.
-          # TODO let's remove this when net-ssh 2.9.2 is old enough, and
-          # bump the required net-ssh version.
-          @forwarded_remote_ports ||= {}
-          actual_local_port, actual_local_host = @forwarded_remote_ports[[remote_port, remote_host]]
-        end
+            actual_local_port, actual_local_host = session.forward.active_local_destinations[[local_port, local_host]]
+          else
+            # If active_local_destinations isn't on net-ssh, we stash our own list
+            # of ports *we* have forwarded on the connection, and hope that we are
+            # right.
+            # TODO let's remove this when net-ssh 2.9.2 is old enough, and
+            # bump the required net-ssh version.
+            @forwarded_remote_ports ||= {}
+            actual_local_port, actual_local_host = @forwarded_remote_ports[[remote_port, remote_host]]
+          end
 
-        if !actual_local_port
-          Chef::Log.debug("Forwarding #{local_host}:#{local_port} to remote server #{remote_host}:#{remote_port} on #{username}@#{self.host}")
-          actual_local_host = local_host
-          actual_local_port = session.forward.local(local_port, local_host, remote_port, remote_host)
-          @forwarded_remote_ports[[remote_port, remote_host]] = [ actual_local_port, actual_local_host ] if @forwarded_remote_ports
-          Chef::Log.info("Forwarded #{actual_local_host}:#{actual_local_port} to remote server #{remote_host}:#{remote_port} on #{username}@#{self.host}")
+          if !actual_local_port
+            Chef::Log.debug("Forwarding #{local_host}:#{local_port} to remote server #{remote_host}:#{remote_port} on #{username}@#{self.host}")
+            actual_local_host = local_host
+            actual_local_port = session.forward.local(local_port, local_host, remote_port, remote_host)
+            @forwarded_remote_ports[[remote_port, remote_host]] = [ actual_local_port, actual_local_host ] if @forwarded_remote_ports
+            Chef::Log.info("Forwarded #{actual_local_host}:#{actual_local_port} to remote server #{remote_host}:#{remote_port} on #{username}@#{self.host}")
+          end
+          [ actual_local_port, actual_local_host ]
         end
-        [ actual_local_port, actual_local_host ]
       end
     end
   end
 end
+end
+
+# I feel so bad about this.  But we need some way to coordinate our loops.
+if !Net::SSH::Connection::Session.method_defined?(:process?)
+  class Net::SSH::Connection::Session
+    def process?(wait=nil)
+      return false unless preprocess
+
+      r = listeners.keys
+      w = r.select { |w2| w2.respond_to?(:pending_write?) && w2.pending_write? }
+      readers, writers, = Net::SSH::Compat.io_select(r, w, nil, io_select_wait(wait))
+
+      postprocess(readers, writers)
+      return !(Array(readers).empty? && Array(writers).empty?)
+    end
+  end
 end
