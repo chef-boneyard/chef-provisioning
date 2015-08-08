911180c5227ca4d658b5626818d657498795d3bc
Chris Doherty <cdoherty@chef.io>
2015-08-08 15:19:26 -0700

    Add a shared RSpec context in chef/provisioning/rspec.rb that drivers can include to easily load the driver for testing.
    - Wrap 'when_the_chef_12_server' in 'with_chef_server' with common defaults (which can be
      overridden).
    - Rake tasks to automate testing the driver generator and specs.
    - Add required version restrictions in the generated Gemfile.
    - Add some basic reality-checking specs to the generated driver, including a loading a new
      no-op resource in the new driver.
    - Add 'rake cycle' to Travis to run the whole generate-test sequence.

---
+++ lib/chef/provisioning/rspec.rb
@@ -0,0 +1,24 @@
+RSpec.shared_context "run with driver" do |driver_args|
+  require 'cheffish/rspec/chef_run_support'
+  extend Cheffish::RSpec::ChefRunSupport
+
+  include_context "with a chef repo"
+
+  driver_object = Chef::Provisioning.driver_for_url(driver_args[:driver_string])
+
+  let(:provisioning_driver) { driver_object }
+
+  # has to be a class method to be available outside of examples.
+  def self.with_chef_server(description = "is running", *options, &block)
+
+    # no need to repeat these every time.
+    args = { organization: "spec_tests", server_scope: :context, port: 8900..9000 }
+    args = args.merge(options.last) if options.last.is_a?(Hash)
+
+    # this ends up in ChefZero::RSpec::RSpecClassMethods#when_the_chef_server, which defines all its code
+    # inside an RSpec context and then runs `instance_eval` on &block--which means it's only available as a
+    # block operator. it's not obviously impossible to factor out the code into a shared_context that we could
+    # include as above with "with a chef repo", but that's a chef-zero patch.
+    when_the_chef_12_server description, args, &block
+  end
+end
