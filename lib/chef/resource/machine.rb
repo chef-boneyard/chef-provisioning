f5f030b99cb32aba84f021bb3c019d724b04fbf1
david raistrick <david.raistrick@fanduel.com>
2015-09-30 19:00:13 -0400

    merge in the right direction...

--- lib/chef/resource/machine.rb
+++ lib/chef/resource/machine.rb
@@ -98,7 +98,7 @@ class Machine < Chef::Resource::LWRPBase
   end
 
   def add_machine_options(options)
-    @machine_options = Chef::Mixin::DeepMerge.hash_only_merge(options, @machine_options)
+    @machine_options = Chef::Mixin::DeepMerge.hash_only_merge(@machine_options, options)
   end
 
   # This is here because provisioning users will probably want to do things like:
